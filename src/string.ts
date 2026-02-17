02416956301b42941ed8abd15805d1b3bbef48c8
madkarmaa <madkarma99@gmail.com>
2026-02-17 10:28:16 +0100

    feat: add more string helpers

--- src/string.ts
+++ src/string.ts
@@ -4,6 +4,10 @@ declare global {
             this: S,
             prefix: P
         ): `${P}${S}`;
+        withSuffix<const S extends string, const SU extends string>(
+            this: S,
+            suffix: SU
+        ): `${S}${SU}`;
     }
 }
 
@@ -12,6 +16,13 @@ String.prototype.withPrefix = function <
     const S extends string
 >(this: S, prefix: P): `${P}${S}` {
     return `${prefix}${this.valueOf()}` as `${P}${S}`;
+};
+
+String.prototype.withSuffix = function <
+    const S extends string,
+    const SU extends string
+>(this: S, suffix: SU): `${S}${SU}` {
+    return `${this.valueOf()}${suffix}` as `${S}${SU}`;
 };
 
 export {};
